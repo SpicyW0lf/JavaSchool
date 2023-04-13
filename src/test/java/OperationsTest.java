600f76e11e779b6adb01f45bfd9b3d6b0294305d
SpicyW0lf <62665736+SpicyW0lf@users.noreply.github.com>
2023-04-13 15:56:18 +0300

    test for createValue done

--- src/test/java/OperationsTest.java
+++ src/test/java/OperationsTest.java
@@ -12,18 +12,22 @@ public class OperationsTest {
         Insert ins = new Insert();
         Map<String, Object> row = new HashMap<>() {
             {
-                put("id", 5);
+                put("id", "5");
                 put("lastName", "Petrov");
-                put("age", 21);
-                put("cost", 2.4);
-                put("active", true);
+                put("age", "21");
+                put("cost", "2.4");
+                put("active", "true");
             }
         };
+        Map<String, Object> ne = null;
         try {
-            assertEquals(row, ins.createValue("'id' = 5, 'lastName' = 'Petrov', 'age' = 21, 'cost' = 2.4" +
-                    " 'active' = true"));
+            ne = ins.createValue("'id' = 5, 'lastName' = 'Petrov', 'age' = 21, 'cost' = 2.4," +
+                    " 'active' = true");
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
+        }
+        for (Map.Entry<String, Object> el : ne.entrySet()) {
+            assertEquals(row.get(el.getKey()), el.getValue());
         }
     }
 }
